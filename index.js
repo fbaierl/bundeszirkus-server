0a80342c0f97de5bd661664da00430d55c1d613d
Florian Baierl <fbaierl1@gmail.com>
2018-07-31 20:58:42 +0200

    Removed unused packages.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 var dataLoader = require('./dataLoader.js')
-var dataScraper = require('./dataScraper.js')
+// var dataScraper = require('./dataScraper.js')
 
 app.use(express.static('public'))
 
