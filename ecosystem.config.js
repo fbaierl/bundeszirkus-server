5e3b1538e815824525dfa347360684d3238107ab
Florian Baierl <fbaierl1@gmail.com>
2019-06-01 22:46:16 +0200

    some cleaning up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
     }
   },
   {
-    "name"        : "Xvfb",
-    "interpreter" : "none",
-    "script"      : "Xvfb",
-    "args"        : ":99"
+    name        : "Xvfb",
+    interpreter : "none",
+    script      : "Xvfb",
+    args        : ":99"
   }],
   deploy: {
     production: {
