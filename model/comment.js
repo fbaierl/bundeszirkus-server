de5aa85a6c3b7fca50eb6d7b8f3a9bf3eeadd3fc
Florian Baierl <fbaierl1@gmail.com>
2020-01-03 20:52:07 +0100

    fixed Dr.h.c bugfix

--- model/comment.js
+++ model/comment.js
@@ -6,6 +6,20 @@ function Comment(fullname, party, text) {
         this.text = text
 }
 
+function cleanUpFullName(raw) {
+    let detectRectangleBracketsAtTheEndOfStringRegex = /(.*)(\[.*?\]$)/g
+    let match = detectRectangleBracketsAtTheEndOfStringRegex.exec(raw)
+    if(match !== null){
+        // if we matched there is a city name after the name (e.g. Carsten Schneider [Erfurt]) so we replace it here
+        raw = raw.replace(match[2].trim(), "").trim()
+    }
+    return raw
+        // ... in case fullname is e.g. "Gegenruf des Abg. Karsten Hilse"
+        .replace(/Gegenrufe? de(s|r) Abg[.]?/, "")
+        // ... in case fullname has a predeceding "Abg" or "Abg."
+        .replace(/Abg.?/,"")
+} 
+
 /**
  * Constructs a Comment object using a text passage from the protocols
  */
@@ -25,15 +39,13 @@ function Comment(raw){
         * Good resource for testing regex: regex101.com
         * 
         * Sometimes the document uses different kinds of whitespaces, so we have to filter them out too...
+        * 
+        * Also comments might look something like this: "Dr.&#xA0;h. c. [Univ Kyiv] Hans Michelbach [CDU/CSU]: Quatsch! Unsinn!"
         */
-       let r = /(?:\(|^)?(.*?) (?:\[.+\] )?\[(.*?)\](?::|, an .+? gewandt:) (.*?)(?:\)|$)/g
+       let r = /(?:\(|^)?(.*) (?:\[.+\] )?\[(.*?)\](?::|, an .+? gewandt:) (.*?)(?:\)|$)/g
        let match = r.exec(raw);
        if(match){
-            let fullname = match[1].trim()
-                        // ... in case fullname is e.g. "Gegenruf des Abg. Karsten Hilse"
-                        .replace(/Gegenrufe? de(s|r) Abg[.]?/, "")
-                        // ... in case fullname has a predeceding "Abg" or "Abg."
-                        .replace(/Abg.?/,"")
+            let fullname = cleanUpFullName(match[1].trim())
             let party = match[2].trim()
             // work-around for a faulty raw xml file (19046-data.xml)
             if(party === "BÜNDNIS 90/D"){
