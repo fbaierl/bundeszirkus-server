63eb2594d5c06019eb00225ff184f7cb0ec14411
Florian Baierl <fbaierl1@gmail.com>
2020-02-08 15:00:28 +0100

    refactored scraping & downloading

---
+++ DataDownloader.js
@@ -0,0 +1,43 @@
+const logger = require('./logger')
+const fs = require('fs')
+var async = require('async');
+const scraperjs = require('scraperjs')
+const url = require('url')
+
+const DATA_DOWNLOAD_DIR = './data/'
+
+class DataDownloader {
+
+	async _downloadFileFromHref(href, callback) {
+	    let fileName = url.parse(href).pathname.split('/').pop();
+	    logger.info("[downloader] downloading file: " + fileName + " from href: " + href + ".")
+	    scraperjs.StaticScraper.create(href)
+            .scrape(function($) {
+                return $.html()
+            })
+            .then(function(data) {
+                fs.writeFileSync(DATA_DOWNLOAD_DIR + fileName, data)
+                logger.info("[downloader] finished writing file " + fileName + ".")
+                callback(undefined)
+            })
+	}
+
+    downloadData(hrefs, callback){
+        const _this = this
+        // parallel map: https://promise-nuggets.github.io/articles/14-map-in-parallel.html
+        async.map(hrefs, function(href, callback) {
+            _this._downloadFileFromHref(href, function (err) {
+                if (err) return callback(err);
+                callback(undefined);
+            })
+        }, function(err) {
+            if(err){
+                logger.error(err)
+            }
+            logger.info("[downloader] finished all downloads.")
+            callback()
+        });
+    }
+}
+
+module.exports = DataDownloader
---
+++ HrefScraper.js
@@ -0,0 +1,77 @@
+const Nightmare = require('nightmare')
+const Xvfb = require('xvfb')
+const logger = require('./logger')
+const os = require('os')
+
+const BT_LINK = "http://www.bundestag.de" // https is NOT supported
+const BT_LINK_OPENDATA = 'https://www.bundestag.de/services/opendata'
+
+const runsOnWindows = (os.platform() === 'win32')
+const useXvfb = !runsOnWindows && !process.argv.includes("noXvfb")
+
+class DataScraper {
+
+	_checkDocumentLink(href) {
+	    // get href; ex: 
+		// (X) /blob/490380/1d83b7e383f9f09d88bd9e89aba07fb0/pp14-data.zip  
+		// (O) /blob/562990/056c28051cb695642cb9d72521bba93b/19044-data.xml
+		// check with regex:
+		// \/blob\/.+?\/.+?\/(\d+)-data.(.*)
+		// filename: <1. capturing group>-data.<2nd capturing group>
+	    const regex = /\/blob\/.+?\/.+?\/(\d+)-data.(.*)/
+	    const match = regex.exec(href)
+	    // check if 2nd capturing group is "xml"
+	    if(match && match[2] && match[2].toLowerCase() === 'xml'){
+	        // construct download link
+			// result has to look like this: https://www.bundestag.de/blob/563398/99a2c9d4df10fcf44e67d084bf6c8f21/19046-data.xml
+	        return href
+	    }
+	    return undefined
+	}
+
+	scrape(callback) {
+		let checkDocumentLink = this._checkDocumentLink
+		
+		if(useXvfb){
+			let xvfb = new Xvfb()
+			try {
+				xvfb.startSync()
+			} catch (e) {
+				logger.error(e)
+			}
+		}
+		let nightmare = new Nightmare({ show: false })
+	    // we request nightmare to browse to the bundestag.de url and extract the whole inner html
+	    nightmare
+	        .goto(BT_LINK_OPENDATA)
+	        .wait(3000)
+			.evaluate(() => Array.from(document.querySelectorAll('.bt-link-dokument')).map(a => a.href)) // return of document.querySelectorAll is not serializable.
+			.end()
+			.then(foundHrefs => {
+				let hrefs = [];
+				foundHrefs.forEach(href => {
+					let candidate = checkDocumentLink(href)
+					if(candidate){
+						hrefs.push(candidate)
+					}
+				})
+				logger.info("[scraper] found " + hrefs.length + " valid links out of " + foundHrefs.length + "links in total.")
+				if(useXvfb){
+					xvfb.stopSync();
+				}
+				callback(undefined, hrefs)
+				return
+			})
+			.catch(err => {
+				if(useXvfb){
+					xvfb.stopSync();  
+				}
+				logger.error("[scraper] did not download any files.")
+				logger.error(err)
+				callback(err, undefined)
+				return
+			})
+	}
+}
+
+module.exports = DataScraper
